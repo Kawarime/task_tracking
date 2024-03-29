f13176d62c77a772dcb534b22bf966483ba5e9e2
Kawarime <kaliak.miroslav@gmail.com>
2024-03-29 18:26:35 +0200

    не работает task_add

--- tasktrack/urls.py
+++ tasktrack/urls.py
@@ -3,4 +3,6 @@ from tasktrack.views import *
 
 urlpatterns = [
     path('', index, name = "index"),
+    path('detail/<int:task_id>/', task_detail, name = "task_detail"),
+    path('task_add/', task_add, name="task_add")
 ]
--- tasktrack/views.py
+++ tasktrack/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from tasktrack.models import *
 
 def index(request):
@@ -6,4 +6,28 @@ def index(request):
     context = {"task":task}
     return render(request, "tasktrack/index.html", context)
 
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+    context = {"task":task}
+    return render(request, "tasktrack/detail.html", context=context)
 
+def task_add(request):
+    if request.method == "POST":
+        task_name = request.POST.get("task_name")
+        task_description = request.POST.get("task_description")
+        task_status = request.POST.get("task_status")
+        task_owner = request.POST.get("task_owner")
+        task_priority = request.POST.get("task_priority")
+        task_deadline = request.POST.get("task_deadline")
+
+        task = Task.objects.create(
+            name=task_name,
+            description=task_description,
+            status=task_status,
+            owner=task_owner,
+            priority=task_priority,
+            deadline = task_deadline
+        )
+        return redirect("task_detail", id=task.id)
+    else:
+        return render(request, template_name="tasktrack/task_add.html")
